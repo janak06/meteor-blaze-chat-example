5576c5172448cfa9e6cfdd74e6aadd1e65be3421
janak06 <janakrathod06@gmail.com>
2022-07-31 16:05:13 +0530

    added emoji picker

--- client/messages.js
+++ client/messages.js
@@ -42,6 +42,25 @@ Template.message.helpers({
 });
 
 Template.message.events({ 
+  'click #emoji-button'(){
+    var el = document.getElementById("msg");
+    el.addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        console.log('1111');
+        event.preventDefault();
+      }
+    });
+    const button = document.querySelector('#emoji-button');
+
+    const picker = new EmojiButton();
+    picker.on('emoji', emoji => {
+      var a = $('#msg').val()
+      a += emoji
+      $('#msg').val(a)
+        // document.querySelector('#msg').value += emoji;
+      });
+      picker.togglePicker(button);
+  },
   'click .get-message'(evt,instance){
 		var id = $(evt.currentTarget).attr('data-index');
     instance.otherUserId.set(id)
@@ -49,6 +68,7 @@ Template.message.events({
 	},
   'submit #send-message' (event,instance) {
 		event.preventDefault();
+    console.log('222');
 		var message = event.target.msg.value;
     var otherUser = instance.otherUserId.get();
 		Meteor.call('addMessage',otherUser, message, (error) => {
